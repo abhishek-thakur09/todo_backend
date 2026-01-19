532e0c8199eca3d140f2ad19ee1b9881274309a3
Abhishek Thakur <abburajput7876@gmail.com>
2026-01-19 07:54:54 +0530

    updation in cache

--- utils/cache.js
+++ utils/cache.js
@@ -1,24 +1,33 @@
 const store = new Map();
 const defaulttime = 60 * 1000;
 
+
+// read data from cache
 function get(key) {
+  // store data in entry
   const entry = store.get(key);
+  // if entry is empty return null
   if (!entry) return null;
+  // if time will exceedes the set time or expiration time then cach will automatically delete
   if (Date.now() > entry.expiresAt) {
     store.delete(key);
     return null;
   }
+  // if above condions are not happened then return the cache data
   return entry.data;
 }
 
-function set(key, data, ttlMs = defaulttime) {
-  store.set(key, { data, expiresAt: Date.now() + ttlMs });
+// store data with expire timestamp
+function set(key, data, expirein = defaulttime) {
+  store.set(key, { data, expiresAt: Date.now() + expirein });
 }
 
 function del(key) {
   store.delete(key);
 }
 
+
+// it is uses for delete the multiple data or in bulk
 function delByPrefix(prefix) {
   for (const k of store.keys()) {
     if (k.startsWith(prefix)) store.delete(k);
